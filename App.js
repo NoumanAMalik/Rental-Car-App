d926596811dfd6f2a8ab3c578b30a55d16d79ae1
Nouman <noumancodes@gmail.com>
2022-10-19 19:31:02 -0400

    Getting car data from api and displaying images

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import axios from "axios";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [carData, setCarData] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const data = await axios.get("https://myfakeapi.com/api/cars/");
+            setCarData([...data.data.cars]);
+            setCarData((prevState) => prevState.splice(970));
+        })();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Image
+                style={styles.image}
+                source={{
+                    uri: "https://picsum.photos/200/",
+                }}
+            />
+            <Image
+                source={{
+                    uri: "https://reactnative.dev/img/tiny_logo.png/",
+                }}
+            />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "start",
+    },
+    image: {
+        width: 200,
+        height: 200,
+        padding: 20,
+    },
 });
