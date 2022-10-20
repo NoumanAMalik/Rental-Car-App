4dacd069e45dd3048d7cf4e4a86c6bcdef58eaa5
Nouman <noumancodes@gmail.com>
2022-10-20 00:49:14 -0400

    Changed View to ScrollView for mobile

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import axios from "axios";
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+    Image,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    SafeAreaView,
+} from "react-native";
 import { useEffect, useState } from "react";
 import Card from "./components/Card";
 
@@ -17,22 +24,27 @@ export default function App() {
     }, []);
 
     return (
-        <View style={styles.container}>
+        <ScrollView
+            style={styles.scrollView}
+            contentContainerStyle={styles.container}
+        >
             {carData &&
                 carData.length === 30 &&
                 carData.map((car) => {
                     return <Card {...car} key={car.id} />;
                 })}
-        </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        flexWrap: "wrap",
         justifyContent: "center",
+        alignItems: "center",
+    },
+    scrollView: {
+        alignSelf: "center",
+        width: "100%",
     },
 });
