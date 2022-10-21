5203501b6c22f6319bc700fbbe32f3fc288814c9
Nouman <noumancodes@gmail.com>
2022-10-20 23:22:20 -0400

    Fixed web layout

--- App.js
+++ App.js
@@ -2,9 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import axios from "axios";
 import {
-    Image,
     StyleSheet,
-    Text,
     View,
     ScrollView,
     useWindowDimensions,
@@ -27,7 +25,7 @@ export default function App() {
     }, []);
 
     return (
-        <View>
+        <View style={styles.appContainer}>
             <View style={styles.search} width={window.width - 20}>
                 <TextInput
                     style={styles.searchBar}
@@ -38,7 +36,7 @@ export default function App() {
             </View>
             <ScrollView
                 style={styles.scrollView}
-                contentContainerStyle={styles.container}
+                contentContainerStyle={styles.scrollContainer}
             >
                 {carData &&
                     carData.length === 30 &&
@@ -46,26 +44,33 @@ export default function App() {
                         let carName =
                             car.car_model_year +
                             " " +
+                            car.car_color +
+                            " " +
                             car.car +
                             " " +
                             car.car_model;
-                        if (carName.includes(search)) {
+                        if (
+                            carName.toLowerCase().includes(search.toLowerCase())
+                        ) {
                             return <Card {...car} key={car.id} />;
                         }
                     })}
             </ScrollView>
+            <StatusBar hidden={true} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    appContainer: {
+        flex: 1,
+    },
+    scrollContainer: {
         backgroundColor: "#fff",
         justifyContent: "center",
         alignItems: "center",
-        // flex: 1,
-        // flexDirection: "row",
-        // flexWrap: "wrap",
+        flexDirection: "row",
+        flexWrap: "wrap",
     },
     scrollView: {
         alignSelf: "center",
@@ -77,13 +82,6 @@ const styles = StyleSheet.create({
         marginHorizontal: 10,
         marginTop: 10,
         backgroundColor: "#ded",
-        // shadowOffset: {
-        //     width: 0,
-        //     height: 5,
-        // },
-        // shadowRadius: 15,
-        // shadowColor: "rgba(0, 0, 0, 0.35)",
-        // shadowOpacity: 1,
         borderRadius: 5,
         borderWidth: 1,
         justifyContent: "center",
